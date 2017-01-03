c256329298205f7cd48efb36c5fa51c7572d7f0a
AnnaUlyanova <a.ulyanova89@gmail.com>
2017-01-04 11:38:11 +1300

    add contact page, fix js in blog posts, fix show tech/cult posts button

--- public/scripts/script-main-page.js
+++ public/scripts/script-main-page.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 
     $("#hide-cult").click(function() {
+    $(".Technical, .Cultural").removeClass('hide');
     $(".Cultural").addClass('hide');
   })
 
   $("#hide-tech").click(function() {
+    $(".Technical, .Cultural").removeClass('hide');
     $(".Technical").addClass('hide');
   })
 
