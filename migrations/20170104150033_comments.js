0dfd31c12c8bf970ffcc8c49741f828044974ace
AnnaUlyanova <a.ulyanova89@gmail.com>
2017-01-16 16:44:53 +1300

    change to interger

--- migrations/20170104150033_comments.js
+++ migrations/20170104150033_comments.js
@@ -1,10 +1,9 @@
-
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('comments', function (table) {
       table.increments('id').primary()
       table.string('name')
       table.string('comment')
-      table.string('post_id').references('blogposts.id')
+      table.integer('post_id').references('blogposts.id')
     })
 };
 
