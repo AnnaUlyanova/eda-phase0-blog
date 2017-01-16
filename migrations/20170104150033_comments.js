30c432c191682233df23720465d1c06c4bb4084d
Rich Churcher <rich.churcher@gmail.com>
2017-01-16 16:03:35 +1300

    Integer, not string

--- migrations/20170104150033_comments.js
+++ migrations/20170104150033_comments.js
@@ -4,7 +4,7 @@ exports.up = function(knex, Promise) {
       table.increments('id').primary()
       table.string('name')
       table.string('comment')
-      table.string('post_id').references('blogposts.id')
+      table.integer('post_id').references('blogposts.id')
     })
 };
 
